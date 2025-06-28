3341341cb6b06eb87804d6386ef0a57f4795fc7d
Sumitsarda2000 <sumitsarda2000@gmail.com>
2025-06-28 22:28:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true, // Required to avoid Image Optimization API errors during static export
-  },
 };
 
 module.exports = nextConfig;
-
